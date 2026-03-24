55d28dbe9314c178a727b2491f349de59258f132
mahindroid <M.singh22@icloud.com>
2026-03-24 19:13:38 -0400

    Delete frontend directory

--- frontend/config.js
+++
@@ -1,7 +0,0 @@
-window.APP_CONFIG = {
-  DATA_MODE: "backend",
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxHJNcIctJTgKCS_QRzUXzFqQvB6CLFRyx0m_qnt4X8lW90NRXX1cVOjhB3tJsUV0L7/exec",
-  // To use Google Sheets instead:
-  // DATA_MODE: "backend",
-  // APPS_SCRIPT_URL: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec"
-};
