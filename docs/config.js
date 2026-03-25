487f4217518475d50abc70427e1a96cd79a6461e
mahindroid <M.singh22@icloud.com>
2026-03-24 21:09:02 -0400

    Add files via upload

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   DATA_MODE: "backend",
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxHJNcIctJTgKCS_QRzUXzFqQvB6CLFRyx0m_qnt4X8lW90NRXX1cVOjhB3tJsUV0L7/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxAivhypOqmVMrHZI0G4yZ9kUp7GfFost3GFxdeWq8T6yE-iQDIoKuuAb3RckZ2SeEu/exec",
   // To use Google Sheets instead:
   // DATA_MODE: "backend",
   // APPS_SCRIPT_URL: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec"
