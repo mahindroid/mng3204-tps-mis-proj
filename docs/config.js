6cca2d42e0f0cc2bd152a3e13eee6acb2c251ce7
mahindroid <M.singh22@icloud.com>
2026-03-24 21:43:59 -0400

    Update app script url

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   DATA_MODE: "backend",
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbypx6LyuBZK-zdRcDNd4pyuNJGhgsRf98qQqA0bbcdc/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzCyjdcFRiLm_2ajGyAa2D4LX-c-KRAm0LuynXjgbin7WkSUYEvRovrRUoFJrSft4KvQA/exec/exec",
   // To use Google Sheets instead:
   // DATA_MODE: "backend",
   // APPS_SCRIPT_URL: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec"
